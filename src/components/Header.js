522f9a70a1147e25e0a158ad7829ac2a6711ffca
jordanavq <jordanavq@gmail.com>
2021-08-14 21:58:47 -0300

    add style to navBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../App.css";
 
 const Header = (props) => {
   return (
-    <nav>
+    <nav
+      style={{ backgroundColor: "#911d47" }}
+      className="navbar align-items-center fs-5 text fw-bold "
+    >
       <Link exact to="/">
-        <img src="../images/logo.jpeg" alt="logo" />
+        <img className="logo-navbar" src="../images/logo.jpeg" alt="logo" />
       </Link>
       {props.active ? (
         <>
-          <Link to="/countries">Countries</Link>
-          <Link to="/grapes">Grapes</Link>
+          <ul className="nav justify-content-center">
+            <Link to="/countries">Countries</Link>
+
+            <Link to="/grapes">Grapes</Link>
+          </ul>
         </>
       ) : null}
     </nav>
