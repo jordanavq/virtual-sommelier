ab39bf46308e80a7e5dbb0639e7a4e1d04a63b40
jordanavq <jordanavq@gmail.com>
2021-08-16 18:49:03 -0300

    color font  changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     <div className="footer">
       <footer
         className="navbar   navbar-static-bottom"
-        style={{ backgroundColor: "black", opacity: 0.9 }}
+        style={{ backgroundColor: "#911d47" }}
       >
         <div className="footer-contacts ">
           <div className="name-container">
@@ -17,10 +17,8 @@ function Footer() {
                 target="_blank"
                 rel="noreferrer"
               >
-                <h6 className="m-0 name-text text-secondary">
-                  JORDANA QUEIROZ{" "}
-                </h6>
-                <FaGithub className="fa-icons mx-1 text-secondary" />
+                <h6 className="m-0 name-text text-light">JORDANA QUEIROZ </h6>
+                <FaGithub className="fa-icons mx-1 text-light" />
               </a>
             </div>
           </div>
@@ -32,20 +30,20 @@ function Footer() {
                 target="_blank"
                 rel="noreferrer"
               >
-                <h6 className="m-0 name-text text-secondary">VICTOR CAMPOS </h6>
-                <FaGithub className="fa-icons mx-1 text-secondary" />
+                <h6 className="m-0 name-text text-light">VICTOR CAMPOS</h6>
+                <FaGithub className="fa-icons mx-1 text-light" />
               </a>
             </div>
           </div>
         </div>
         <div className="developed">
-          <p className=" m-0 text-secondary">
+          <p className=" m-0 text-light">
             <small>
               Developed by: <strong>Jordana Queiroz</strong> &{" "}
               <strong>Victor Campos</strong>
             </small>
           </p>
-          <p className=" m-0 text-secondary">
+          <p className=" m-0 text-light">
             <small>© 2021 Copyright: JordanaQueiroz/VictorCampos</small>
           </p>
         </div>
