4aae354c0b2d0c41313076939ebab96a3aedce51
jordanavq <jordanavq@gmail.com>
2021-08-16 19:59:45 -0300

    organize components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../App.css";
-import logo from "../images/logo4.png";
+import "./Header.css";
+import logo from "../../images/logo4.png";
 
 const Header = (props) => {
   return (
