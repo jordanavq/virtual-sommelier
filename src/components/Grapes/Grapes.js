3b6ab59b7142e6140da4a593b4b384d4a3f75062
Victor Campos <vccacampos@outlook.com>
2021-08-17 12:35:07 -0300

    Grapes card e Grapes

---
+++ src/components/Grapes/Grapes.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Grapes.css"
+
+const Grapes = () => {
+  return (
+      <div>
+          <h1>Search by Grapes</h1>
+      </div>,
+    <div>
+      <bottom>Chardonnay</bottom>
+      <bottom>Sauvignon Blanc</bottom>
+      <bottom>Malbec</bottom>
+      <bottom>Cabernet Sauvignon</bottom>
+      <bottom>Primitivo</bottom>
+      <bottom>Tempranillo</bottom>
+      <bottom>Shiraz</bottom>
+      <bottom>Merlot</bottom>
+      <bottom>Pinot Noir</bottom>
+
+    </div>
+  );
+};
+
+export default Grapes;
