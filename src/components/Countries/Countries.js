e89c1530e6af39753a9257b45a72828930a8eec5
jordanavq <jordanavq@gmail.com>
2021-08-19 19:35:58 -0300

    update

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,9 +1,61 @@
 import React from "react";
+import "./Countries.css";
 
 const Countries = () => {
   return (
     <div>
-      <h1>Teste</h1>
+      <h1>Search by Countries</h1>
+      <ul className="d-flex justify-content-around">
+        <li>
+          <button type="button" className="btn btn-outline-danger">
+            Cabernet Sauvignon
+          </button>
+        </li>
+        <li>
+          <button type="button" className="btn btn-outline-danger">
+            Chardonnay
+          </button>
+        </li>
+        <li>
+          <button type="button" className="btn btn-outline-danger">
+            Malbec
+          </button>
+        </li>
+      </ul>
+      <ul className="d-flex justify-content-around">
+        <li>
+          <button type="button" className="btn btn-outline-danger">
+            Merlot
+          </button>
+        </li>
+        <li>
+          <button type="button" className="btn btn-outline-danger">
+            Primitivo
+          </button>
+        </li>
+        <li>
+          <button type="button" className="btn btn-outline-danger">
+            Pinot Noir
+          </button>
+        </li>
+      </ul>
+      <ul className="d-flex justify-content-around">
+        <li>
+          <button type="button" className="btn btn-outline-danger">
+            Sauvignon Blanc
+          </button>
+        </li>
+        <li>
+          <button type="button" className="btn btn-outline-danger">
+            Shiraz
+          </button>
+        </li>
+        <li>
+          <button type="button" className="btn btn-outline-danger">
+            Tempranillo
+          </button>
+        </li>
+      </ul>
     </div>
   );
 };
