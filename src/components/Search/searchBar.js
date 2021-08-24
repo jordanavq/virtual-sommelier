a0b8688a48ec12609efbba3992a729bdb15e6d2f
Victor Campos <vccacampos@outlook.com>
2021-08-24 15:24:50 -0300

    Search Bar start

---
+++ src/components/Search/searchBar.js
@@ -0,0 +1,12 @@
+import "./searchBar.css";
+import React from "react";
+
+const Search = (props) => {
+  return (
+    <div className="SearchBar">
+     
+    </div>
+  );
+};
+
+export default Search;
